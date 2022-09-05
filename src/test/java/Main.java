0cf6c3d37b9cb1cd013cfe8aea565d39bb84846c
cristian millan <cristianjhoan49@gmail.com>
2022-09-05 17:09:39 -0500

    Version 1.1: Se añadio un try catch para manejar la excepcion de tipo NoSuchFindWebElement, es decir cuando no se encuentra un Web Element

--- src/test/java/Main.java
+++ src/test/java/Main.java
@@ -1,9 +1,6 @@
 
+import org.openqa.selenium.*;
 import org.openqa.selenium.support.ui.*;
-import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterTest;
@@ -34,68 +31,74 @@ public class Main {
     }
     @Test
     public void hacer_una_busqueda() throws InterruptedException {
+        try{
+            //Abrimos una URL
+            chromeDriver.get("https://schoolpack.smart.edu.co/idiomas/alumnos.aspx");
 
-        //Abrimos una URL
-        chromeDriver.get("https://schoolpack.smart.edu.co/idiomas/alumnos.aspx");
+            //Mapeamos los elementos para hacer login
+            WebElement user = chromeDriver.findElement(By.id("vUSUCOD"));
+            user.sendKeys("1005058588");
 
-        //Mapeamos los elementos para hacer login
-        WebElement user = chromeDriver.findElement(By.id("vUSUCOD"));
-        user.sendKeys("1005058588");
+            WebElement password = chromeDriver.findElement(By.id("vPASS"));
+            password.sendKeys("Cmillan1224");
+            password.sendKeys(Keys.ENTER);
 
-        WebElement password = chromeDriver.findElement(By.id("vPASS"));
-        password.sendKeys("Cmillan1224");
-        password.sendKeys(Keys.ENTER);
+            //Espera hasta que el elemento sea visible
+            WebDriverWait esperaExplicita = new WebDriverWait(chromeDriver,Duration.ofSeconds(60));
+            esperaExplicita.until(ExpectedConditions.visibilityOfElementLocated(By.id("IMAGE18")));
 
-        //Espera hasta que el elemento sea visible
-        WebDriverWait esperaExplicita = new WebDriverWait(chromeDriver,Duration.ofSeconds(60));
-        esperaExplicita.until(ExpectedConditions.visibilityOfElementLocated(By.id("IMAGE18")));
+            WebElement programacion = chromeDriver.findElement(By.id("IMAGE18"));
+            programacion.click();
 
-        WebElement programacion = chromeDriver.findElement(By.id("IMAGE18"));
-        programacion.click();
+            //esperaExplicita.until(ExpectedConditions.visibilityOfElementLocated(By.id("W0030Grid1ContainerRow_0001")));
+            WebElement planDeEstudios = chromeDriver.findElement(By.id("W0030Grid1ContainerRow_0001"));
+            planDeEstudios.click();
 
-        //esperaExplicita.until(ExpectedConditions.visibilityOfElementLocated(By.id("W0030Grid1ContainerRow_0001")));
-        WebElement planDeEstudios = chromeDriver.findElement(By.id("W0030Grid1ContainerRow_0001"));
-        planDeEstudios.click();
+            WebElement botonIniciar = chromeDriver.findElement(By.id("W0030BUTTON1"));
+            botonIniciar.click();
 
-        WebElement botonIniciar = chromeDriver.findElement(By.id("W0030BUTTON1"));
-        botonIniciar.click();
+            esperaExplicita.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//iframe[@id='gxp0_ifrm']")));
+            WebElement iframe = chromeDriver.findElement(By.xpath("//iframe[@id='gxp0_ifrm']"));
+            chromeDriver.switchTo().frame(iframe);
 
-        esperaExplicita.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//iframe[@id='gxp0_ifrm']")));
-        WebElement iframe = chromeDriver.findElement(By.xpath("//iframe[@id='gxp0_ifrm']"));
-        chromeDriver.switchTo().frame(iframe);
+            Select listaDesplegable = new Select(chromeDriver.findElement(By.id("vTPEAPROBO")));
+            listaDesplegable.selectByVisibleText("Pendientes por programar");
 
-        Select listaDesplegable = new Select(chromeDriver.findElement(By.id("vTPEAPROBO")));
-        listaDesplegable.selectByVisibleText("Pendientes por programar");
+            //Espera hasta que el elemento sea visible
+            esperaExplicita.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//table[@id='TABLE1']")));
+            WebElement fila1 = chromeDriver.findElement(By.xpath("//tbody/tr[@id='Grid1ContainerRow_0001']/td[6]"));
+            fila1.click();
 
-        //Espera hasta que el elemento sea visible
-        esperaExplicita.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//table[@id='TABLE1']")));
-        WebElement fila1 = chromeDriver.findElement(By.xpath("//tbody/tr[@id='Grid1ContainerRow_0001']/td[6]"));
-        fila1.click();
+            Thread.sleep(2000);
+            WebElement botonAsignar = chromeDriver.findElement(By.xpath("//input[@id='BUTTON1']"));
+            Thread.sleep(2000);
+            botonAsignar.click();
 
-        Thread.sleep(2000);
-        WebElement botonAsignar = chromeDriver.findElement(By.xpath("//input[@id='BUTTON1']"));
-        Thread.sleep(2000);
-        botonAsignar.click();
-
-        Thread.sleep(2000);
-        chromeDriver.switchTo().defaultContent();
-        int size = chromeDriver.findElements(By.tagName("iframe")).size();
-        System.out.println(size);
-        chromeDriver.switchTo().frame(1);
+            Thread.sleep(2000);
+            chromeDriver.switchTo().defaultContent();
+            int size = chromeDriver.findElements(By.tagName("iframe")).size();
+            System.out.println(size);
+            chromeDriver.switchTo().frame(1);
 
 
-        Select listaDesplegableDia = new Select(chromeDriver.findElement(By.id("vDIA")));
-        listaDesplegableDia.selectByIndex(1);
+            Select listaDesplegableDia = new Select(chromeDriver.findElement(By.id("vDIA")));
+            listaDesplegableDia.selectByIndex(1);
 
-        WebElement horaSeleccionar = chromeDriver.findElement(By.xpath("//tbody/tr[@id='Grid1ContainerRow_0009']/td[3]"));
-        horaSeleccionar.click();
+            WebElement horaSeleccionar = chromeDriver.findElement(By.xpath("//tbody/tr[@id='Grid1ContainerRow_0009']/td[3]"));
+            horaSeleccionar.click();
 
-        WebElement confirmar = chromeDriver.findElement(By.cssSelector("#BUTTON1"));
-        confirmar.click();
+            WebElement confirmar = chromeDriver.findElement(By.cssSelector("#BUTTON1"));
+            confirmar.click();
 
-        WebElement mensajeValidar = chromeDriver.findElement(By.cssSelector("body.Form:nth-child(2) form.Form:nth-child(1) div.gx-content-placeholder:nth-child(2) div.gx-ct-body.Form-fx table.gx-tab-spacing-fix-2.Table:nth-child(2) td.gx-tab-padding-fix-1 table.gx-tab-spacing-fix-2.Table tr:nth-child(3) td.gx-tab-padding-fix-1 div:nth-child(1) span.ErrorViewer.gx_ev.ErrorViewerBullet > div.gx-warning-message"));
-        if(mensajeValidar.isDisplayed() && i<2){
-            i++;
+            WebElement mensajeValidar = chromeDriver.findElement(By.cssSelector("body.Form:nth-child(2) form.Form:nth-child(1) div.gx-content-placeholder:nth-child(2) div.gx-ct-body.Form-fx table.gx-tab-spacing-fix-2.Table:nth-child(2) td.gx-tab-padding-fix-1 table.gx-tab-spacing-fix-2.Table tr:nth-child(3) td.gx-tab-padding-fix-1 div:nth-child(1) span.ErrorViewer.gx_ev.ErrorViewerBullet > div.gx-warning-message"));
+            if(mensajeValidar.isDisplayed() && i<2){
+                i++;
+                cerrarDriver();
+                abrirDriver();
+                hacer_una_busqueda();
+            }
+        }catch (NoSuchElementException e){
+            System.out.println("No encontro un WebElement");
             cerrarDriver();
             abrirDriver();
             hacer_una_busqueda();
