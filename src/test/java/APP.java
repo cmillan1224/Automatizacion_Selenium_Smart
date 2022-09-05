8f5ca9133b9871daa46f17f2550b94040d3e9f87
cristian millan <cristianjhoan49@gmail.com>
2022-09-05 16:11:43 -0500

    Version 1: Contiene la automatizacion unicamente con selenium, Junit y webDriver

---
+++ src/test/java/APP.java
@@ -0,0 +1,8 @@
+public class APP {
+    public static void main(String []args) throws InterruptedException {
+        Main test = new Main();
+        test.abrirDriver();
+        test.hacer_una_busqueda();
+
+    }
+}
